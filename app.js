5e97c307b39d2abae362788098dd869475d305f7
Neru <nenriquev1@gmail.com>
2022-08-23 23:08:42 +0200

    Agregando desafio 3

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(path.resolve('./views/home.html'))
 })
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve('./views/register.html'))
+  console.log(req)
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve('./views/login.html'))
+  console.log(req)
+})
